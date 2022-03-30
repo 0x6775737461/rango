f6f1c076d8c9b770c912ad0ea28dcb8927a55257
0x6775737461 <fabiogustavo@tuta.io>
2022-03-30 15:16:36 -0400

    modelo das tabelas "Region" e "Fruits"

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Models):
+    name = models.CharField(max_length=50)
+
+class Fruits(models.Models):
+    name = models.CharField(max_length=25)
+    # o segundo argumento é para evitar a remoção manual
+    origin = models.ForeignKey(to=Region, on_delete=models.CASCADE)
