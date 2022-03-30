fd9caa539e74838933407d2cef28f50b82b02143
0x6775737461 <fabiogustavo@tuta.io>
2022-03-30 16:18:48 -0400

    serializers e o método get

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from app.models import Region, Fruits
+from app.serializers import RegionSerializer, FruitsSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def RegionList(request):
+    # pegando todas as regiões do br
+    regions = Region.object.all()
+
+    serializer = RegionSerializer(regions, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def FruitsList(request):
+    # pegando todos os (as) objetos (frutas) do db
+    fruits = Fruits.objects.all()
+
+    serializer = FruitsSerializer(fruits, many=True)
+
+    return Response(serializer.data)
