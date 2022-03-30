a7f25f89a8713319949762bca5a9d93401f29239
0x6775737461 <fabiogustavo@tuta.io>
2022-03-30 15:20:51 -0400

    nomenclatura correta sem o "s"

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Region(models.Models):
+class Region(models.Model):
     name = models.CharField(max_length=50)
 
-class Fruits(models.Models):
+class Fruits(models.Model):
     name = models.CharField(max_length=25)
     # o segundo argumento é para evitar a remoção manual
     origin = models.ForeignKey(to=Region, on_delete=models.CASCADE)
