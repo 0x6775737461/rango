4dc14609a577c8bbdd5316a0c25faf9982efe492
0x6775737461 <fabiogustavo@tuta.io>
2022-03-31 19:37:22 -0400

    utilizando o nome especificado pelo enunciado do desafio

--- app/models.py
+++ app/models.py
@@ -3,7 +3,13 @@ from django.db import models
 class Region(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        db_table = "Region"
+
 class Fruits(models.Model):
     name = models.CharField(max_length=25)
     # o segundo argumento é para evitar a remoção manual
     origin = models.ForeignKey(to=Region, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "Fruits"
