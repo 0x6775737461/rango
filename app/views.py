b9d9266ca79ce241afdf77cf9f6e029a323d8c5a
0x6775737461 <fabiogustavo@tuta.io>
2022-03-31 09:01:40 -0400

    método post implementado para region

--- app/views.py
+++ app/views.py
@@ -4,14 +4,28 @@ from app.serializers import RegionSerializer, FruitsSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
+from rest_framework import status
+
+@api_view(['GET', 'POST'])
 def RegionList(request):
-    # pegando todas as regiões do br
-    regions = Region.objects.all()
 
-    serializer = RegionSerializer(regions, many=True)
+    if request.method == 'GET':
+        # pegando todas as regiões do br
+        regions = Region.objects.all()
 
-    return Response(serializer.data)
+        serializer = RegionSerializer(regions, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = RegionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # caso a requisição tenha sido feita incorretamente
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def FruitsList(request):
--- readme.md
+++ readme.md
@@ -6,3 +6,5 @@
 - [ ] remover o db e o arquivo de migrations do gitignore.
 
 - [ ] os métodos http estão repetindo em RegionList e Fruits list.
+
+- [ ] há um bug no post quando feito via web api, se fizer pelo curl não ocorre
